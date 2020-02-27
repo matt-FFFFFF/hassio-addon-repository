b1a439106c6d5b7518b91717fe86a4ae1c62242d
Matt White <matt.white@microsoft.com>
2020-02-27 23:30:14 +0000

    Fix DB name

--- setoptions.sh
+++ setoptions.sh
@@ -2,6 +2,6 @@
 
 export POSTGRES_USER=$(bashio::config 'user')
 export POSTGRES_PASSWORD=$(bashio::config 'password')
-export POSTGRES_DATABASE=$(bashio::config 'database')
+export POSTGRES_DB=$(bashio::config 'database')
 
 /usr/local/bin/docker-entrypoint.sh $1
